f99b01e065402980daab93e0f12abe7489781be4
Leonardo Aguilera <leonardolag1268@gmail.com>
2025-03-29 14:40:49 -0600

    Create CoreConcept component, add data.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-// Important:
-// For this project to work on CodeSandbox, image assets ("assets") folder
-// must be stored in the public folder (as it's the case by default in this project)
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  return  (
+    <header>
+    <img src={reactImg} alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+  )
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <img src="assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+            title="Components" 
+            description="The core UI building block"
+            image={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
